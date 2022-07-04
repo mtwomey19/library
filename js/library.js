db2ef0e666fb492e3fea9a83ebe2b2174fec9838
mtwomey19 <mtwomey19@gmail.com>
2022-07-04 08:04:53 -0400

    Turn new book into card

--- js/library.js
+++ js/library.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let newBooks = [];
+// localStorage.clear();
 
 function Book(title, author, numPages, haveRead) {
     this.title = title;
@@ -17,32 +19,39 @@ myLibrary.push(book1, book2, book3);
 
 function goToNewBookPage() {
     const newBookBtn = document.getElementById('new-book-btn');
-    if (newBookBtn === null) {
-    
-    } else {
+    if (newBookBtn !== null) {
         newBookBtn.addEventListener('click', () => {
-        location.href = "./new-book.html"; 
+        location.href = './new-book.html';
+        });
+    }
+}
+
+function goToLibraryPage() {
+    const libBtn = document.getElementById('lib-btn');
+    if (libBtn !== null) {
+        libBtn.addEventListener('click', () => {
+            location.href = './index.html';
         });
     }
 }
 
 goToNewBookPage();
+goToLibraryPage();
 
 // save form data on submission
 function submitButtonClicked() {
     const submitBtn = document.getElementById('submit-btn');
-    if (submitBtn === null) {
-    
-    } else {
-    submitBtn.addEventListener('click', () => {
-        const title = getNewBookInfo('title');
-        const author = getNewBookInfo('author');
-        const numPages = getNewBookInfo('num-pages');
-        const haveRead = getNewBookInfo('have-read');
-        const newBook = new Book(title, author, numPages, haveRead);
-        myLibrary.push(newBook);
-        console.log(myLibrary);
-    });
+    if (submitBtn !== null) {
+        submitBtn.addEventListener('click', () => {
+            const title = getNewBookInfo('title');
+            const author = getNewBookInfo('author');
+            const numPages = getNewBookInfo('num-pages');
+            const haveRead = getNewBookInfo('have-read');
+            
+            const newBook = new Book(title, author, numPages, haveRead);
+            localStorage.setItem('count', +localStorage.getItem('count') + +1);
+            localStorage.setItem(localStorage.getItem('count'), JSON.stringify(newBook));
+        });
     }
 }
 
@@ -55,38 +64,62 @@ function getNewBookInfo(elementId) {
 }
 
 submitButtonClicked();
-console.log(myLibrary);
 
+function addNewBookToLibrary() {
+    for (let i = 1; i <= localStorage.getItem('count'); i++) {
+        let newBook = localStorage.getItem(localStorage.getItem('count'));
+        newBook = JSON.parse(newBook);
+        myLibrary.push(newBook);
+    }
+}
+
+addNewBookToLibrary();
+console.log(myLibrary);
 
 // Convert book data from myLibrary array into cards
 
 const bookCardContainer = document.getElementById('book-card-container');
+let cardCount = 1;
 
 myLibrary.forEach(book => {
-    let bookCard = createBookCard();
-
+    let bookCard = createBookCard(cardCount);
+    cardCount++;
     for (const key in book) {
         if (key === 'title') {
-            const rowTitle = addCellText('Title');
-            bookCard.appendChild(rowTitle);
-            const bookInfo = addCellText(book[key]);
-            bookCard.appendChild(bookInfo);
+            createRow('Title', book[key], bookCard);
+        }
+        if (key === 'author') {
+            createRow('Author', book[key], bookCard);
+        }
+        if (key === 'numPages') {
+            createRow('No. Pages', book[key], bookCard);
+        }
+        if (key === 'haveRead') {
+            createRow('Completed', book[key], bookCard);
         }
     }
 });
 
-function createBookCard() {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    bookCardContainer.appendChild(bookCard);
-    return bookCard;
+function createBookCard(cardCount) {
+    if (bookCardContainer !== null) {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.setAttribute('id', cardCount);
+        bookCardContainer.appendChild(bookCard);
+        return bookCard;
+    }
 }
 
-// Consider adding class name as a paramter to this function
-function addCellText(cellText) {
-    const para = document.createElement('p');
-    const text = document.createTextNode(cellText);
-    para.appendChild(text);
-    return para;
+function createRow(rowTitle, cellText, bookCard) {
+    if (bookCardContainer !== null) {
+        const rowTitleElement = document.createElement('p');
+        const rowTitleText = document.createTextNode(rowTitle);
+        rowTitleElement.appendChild(rowTitleText);
+        bookCard.appendChild(rowTitleElement);
+        
+        const bookInfoElement = document.createElement('p');
+        const bookInfoText = document.createTextNode(cellText);
+        bookInfoElement.appendChild(bookInfoText);
+        bookCard.appendChild(bookInfoElement);
+    }
 }
-
